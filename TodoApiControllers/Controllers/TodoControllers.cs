fbde3aa8c2a158d13c9b0b98237b78c1a5b9faff
Nick <nsagias@gmail.com>
2022-01-08 23:36:54 -0500

    Create controller class and add comments for controllers

--- TodoApiControllers/Controllers/TodoControllers.cs
+++ TodoApiControllers/Controllers/TodoControllers.cs
@@ -3,3 +3,16 @@ using TodoApiControllers.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TodoApiControllers.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TodoController : ControllerBase {
+  public TodoController () {
+  }
+
+  // GET all todos
+  // GET todo by id
+  // POST  create new 
+  // PUT update by id
+  // DELETE by id 
+}
