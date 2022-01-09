e80396e74594bef98a1b9afc9bc394cca227809f
Nick <nsagias@gmail.com>
2022-01-08 22:47:44 -0500

    Rename to TodoItem

--- TodoApiControllers/Models/TodoItem.cs
+++ TodoApiControllers/Models/TodoItem.cs
@@ -1,6 +1,6 @@
 namespace TodoApiControllers.Models;
 
-public class Todo {
+public class TodoItem {
   public int Id {get; set;}
   public string? Name { get; set;}
   public bool IsDone { get; set;}
