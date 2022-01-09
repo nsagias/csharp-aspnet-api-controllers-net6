833956b0fd135a55d88ffc1acce412f03e3e9e5a
Nick <nsagias@gmail.com>
2022-01-08 22:50:25 -0500

    create mock data

--- TodoApiControllers/Services/TodoService.cs
+++ TodoApiControllers/Services/TodoService.cs
@@ -1,1 +1,14 @@
 using TodoApiControllers.Models;
+
+namespace TodoApiControllers.Services;
+
+public static class TodoService {
+  static List<TodoItem> TodosList { get; }
+  static int nexId = 3;
+  static TodoService() {
+    TodosList = new List<TodoItem> {
+      new TodoItem { Id = 1, Name = "Get Api Controller complete", IsDone = false},
+      new TodoItem { Id = 2, Name = "Review entity frameworks", IsDone = false}
+    };
+  }
+}
